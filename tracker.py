b8c992ea6f41de621b27d3678f42eb1857f58360
Franton Lin <franton.lin@students.olin.edu>
2018-03-14 19:44:27 -0400

    Added video output to trackers and renamed markers to pivot and bob

--- tracker.py
+++ tracker.py
@@ -4,93 +4,98 @@ import csv
 import colorsys
 
 # Variables to change
-filename = "videos/antiphase.MP4"
-blueRGB = np.array([65, 113, 162])
-orangeRGB = np.array([217, 175, 87])
-pinkRGB = np.array([198, 90, 124])
-framerate = 30
+videoFilename = "videos/antiphase.MP4"
+bobLRGB = np.array([65, 113, 162])
+bobRRGB = np.array([])
+pivotLRGB = np.array([217, 175, 87])
+pivotRRGB = np.array([198, 90, 124])
+framerate = 60
+dataFilename = 'data/singlepositions.csv'
+videoOutputFilename = 'output.mp4'
+
 
 # Color bounds in HSV
 maxHSV = np.array([179, 255, 255])
 minHSV = np.array([0, 0, 0])
 
-blueHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(blueRGB/255.))), maxHSV)).astype(int)
-blueLowerBound = np.maximum(blueHSV - 40, minHSV)
-blueUpperBound = np.minimum(blueHSV + 40, maxHSV)
+bobLHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(bobLRGB/255.))), maxHSV)).astype(int)
+bobLLowerBound = np.maximum(bobLHSV - 40, minHSV)
+bobLUpperBound = np.minimum(bobLHSV + 40, maxHSV)
 
-orangeHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(orangeRGB/255.))), maxHSV)).astype(int)
-orangeLowerBound = np.maximum(orangeHSV - 40, minHSV)
-orangeUpperBound = np.minimum(orangeHSV + 40, maxHSV)
+bobRHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(bobRRGB/255.))), maxHSV)).astype(int)
+bobRLowerBound = np.maximum(bobRHSV - 40, minHSV)
+bobRUpperBound = np.minimum(bobRHSV + 40, maxHSV)
 
-pinkHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(pinkRGB/255.))), maxHSV)).astype(int)
-pinkLowerBound = np.maximum(pinkHSV - 40, minHSV)
-pinkUpperBound = np.minimum(pinkHSV + 40, maxHSV)
+pivotLHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(pivotLRGB/255.))), maxHSV)).astype(int)
+pivotLLowerBound = np.maximum(pivotLHSV - 40, minHSV)
+pivotLUpperBound = np.minimum(pivotLHSV + 40, maxHSV)
+
+pivotRHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(pivotRRGB/255.))), maxHSV)).astype(int)
+pivotRLowerBound = np.maximum(pivotRHSV - 40, minHSV)
+pivotRUpperBound = np.minimum(pivotRHSV + 40, maxHSV)
 
 
 # Initialize capture, data recording, and writer
-cap = cv2.VideoCapture(filename)
+cap = cv2.VideoCapture(videoFilename)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-data = []
 fourcc = cv2.VideoWriter_fourcc('a', 'v', 'c', '1') # Define the codec
-video = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width,height))
+video = cv2.VideoWriter('output.mp4', fourcc, framerate, (width,height))
+data = []
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     if(frame is None):
         break
-    # height, width = frame.shape[:2]
 
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) > 30:
-        break
+    # if cap.get(cv2.CAP_PROP_POS_FRAMES) > 30:
+    #     break
 
     # Create masks for each colored marker
     frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    blueMask = cv2.inRange(frameHSV, blueLowerBound, blueUpperBound)
-    orangeMask = cv2.inRange(frameHSV, orangeLowerBound, orangeUpperBound)
-    pinkMask = cv2.inRange(frameHSV, pinkLowerBound, pinkUpperBound)
+    bobLMask = cv2.inRange(frameHSV, bobLLowerBound, bobLUpperBound)
+    bobRMask = cv2.inRange(frameHSV, bobRLowerBound, bobRUpperBound)
+    pivotLMask = cv2.inRange(frameHSV, pivotLLowerBound, pivotLUpperBound)
+    pivotRMask = cv2.inRange(frameHSV, pivotRLowerBound, pivotRUpperBound)
 
     # Image filtering
     kernalOpen = np.ones((width//100,width//100))
     kernalClose = np.ones((width//20,width//20))
-    blueMaskOpen = cv2.morphologyEx(blueMask,cv2.MORPH_OPEN, kernalOpen)
-    blueMaskClose = cv2.morphologyEx(blueMaskOpen,cv2.MORPH_CLOSE, kernalClose)
-    orangeMaskOpen = cv2.morphologyEx(orangeMask,cv2.MORPH_OPEN, kernalOpen)
-    orangeMaskClose = cv2.morphologyEx(orangeMaskOpen,cv2.MORPH_CLOSE, kernalClose)
-    pinkMaskOpen = cv2.morphologyEx(pinkMask,cv2.MORPH_OPEN, kernalOpen)
-    pinkMaskClose = cv2.morphologyEx(pinkMaskOpen,cv2.MORPH_CLOSE, kernalClose)
+    bobLMaskOpen = cv2.morphologyEx(bobLMask,cv2.MORPH_OPEN, kernalOpen)
+    bobLMaskClose = cv2.morphologyEx(bobLMaskOpen,cv2.MORPH_CLOSE, kernalClose)
+    bobRMaskOpen = cv2.morphologyEx(bobRMask,cv2.MORPH_OPEN, kernalOpen)
+    bobRMaskClose = cv2.morphologyEx(bobRMaskOpen,cv2.MORPH_CLOSE, kernalClose)
+    pivotLMaskOpen = cv2.morphologyEx(pivotLMask,cv2.MORPH_OPEN, kernalOpen)
+    pivotLMaskClose = cv2.morphologyEx(pivotLMaskOpen,cv2.MORPH_CLOSE, kernalClose)
+    pivotRMaskOpen = cv2.morphologyEx(pivotRMask,cv2.MORPH_OPEN, kernalOpen)
+    pivotRMaskClose = cv2.morphologyEx(pivotRMaskOpen,cv2.MORPH_CLOSE, kernalClose)
 
-    blueContours = cv2.findContours(blueMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    orangeContours = cv2.findContours(orangeMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    pinkContours = cv2.findContours(pinkMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    blueMoments = cv2.moments(blueContours[0])
-    orangeMoments1 = cv2.moments(orangeContours[1][0])
-    orangeMoments2 = cv2.moments(orangeContours[1][1])
-    pinkMoments = cv2.moments(pinkContours[0])
+    bobLContours = cv2.findContours(bobLMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    bobRContours = cv2.findContours(bobRMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    pivotLContours = cv2.findContours(pivotLMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    pivotRContours = cv2.findContours(pivotRMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    bobLMoments = cv2.moments(bobLContours[0])
+    bobRMoments1 = cv2.moments(bobRContours[0])
+    pivotLMoments = cv2.moments(pivotLContours[0])
+    pivotRMoments = cv2.moments(pivotRContours[0])
 
     # Calculate centroids
-    blueCentroid = (int(blueMoments['m10']/blueMoments['m00']),int(blueMoments['m01']/blueMoments['m00']))
-    orangeCentroid1 = (int(orangeMoments1['m10']/orangeMoments1['m00']),int(orangeMoments1['m01']/orangeMoments1['m00']))
-    orangeCentroid2 = (int(orangeMoments2['m10']/orangeMoments2['m00']),int(orangeMoments2['m01']/orangeMoments2['m00']))
-    pinkCentroid = (int(pinkMoments['m10']/pinkMoments['m00']),int(pinkMoments['m01']/pinkMoments['m00']))
-
-    if orangeCentroid1[0] > orangeCentroid2[0]:
-        orangeCentroidR = orangeCentroid1
-        orangeCentroidL = orangeCentroid2
-    else:
-        orangeCentroidR = orangeCentroid2
-        orangeCentroidL = orangeCentroid1
+    bobLCentroid = (int(bobLMoments['m10']/bobLMoments['m00']),int(bobLMoments['m01']/bobLMoments['m00']))
+    bobRCentroid = (int(bobRMoments['m10']/bobRMoments['m00']),int(bobRMoments['m01']/bobRMoments['m00']))
+    pivotLCentroid = (int(pivotLMoments['m10']/pivotLMoments['m00']),int(pivotLMoments['m01']/pivotLMoments['m00']))
+    pivotRCentroid = (int(pivotRMoments['m10']/pivotRMoments['m00']),int(pivotRMoments['m01']/pivotRMoments['m00']))
 
     # Create frame with marked centroids
     centroidim = frame.copy()
-    cv2.circle(centroidim, blueCentroid, 3, (0,0,0), -1)
-    cv2.circle(centroidim, orangeCentroid1, 3, (0,0,0), -1)
-    cv2.circle(centroidim, orangeCentroid2, 3, (0,0,0), -1)
-    cv2.circle(centroidim, pinkCentroid, 3, (0,0,0), -1)
-    cv2.drawContours(centroidim, blueContours[1], -1, (0,0,0), 2)
-    cv2.drawContours(centroidim, orangeContours[1], -1, (0,0,0), 2)
-    cv2.drawContours(centroidim, pinkContours[1], -1, (0,0,0), 2)
+    cv2.circle(centroidim, bobLCentroid, 3, (0,0,0), -1)
+    cv2.circle(centroidim, bobRCentroid, 3, (0,0,0), -1)
+    cv2.circle(centroidim, pivotLCentroid, 3, (0,0,0), -1)
+    cv2.circle(centroidim, pivotRCentroid, 3, (0,0,0), -1)
+    cv2.drawContours(centroidim, bobLContours[1], -1, (0,0,0), 2)
+    cv2.drawContours(centroidim, bobRContours[1], -1, (0,0,0), 2)
+    cv2.drawContours(centroidim, pivotLContours[1], -1, (0,0,0), 2)
+    cv2.drawContours(centroidim, pivotRContours[1], -1, (0,0,0), 2)
 
     # Display frame for review
     cv2.namedWindow('review', cv2.WINDOW_NORMAL)
@@ -107,28 +112,24 @@ while(cap.isOpened()):
     cv2.imshow("review", cv2.resize(centroidim, (0, 0), None, .5, .5))
     cv2.waitKey(1)
 
-    # write frames to video
+    # Write frames to video
     if ret == True:
-
-        # Write the frame into the file 'output.avi'
         video.write(centroidim)
-
-    else:
-        break
+        print('.', end='', flush=True)
 
     # Append positions for this frame to the data
     time = (cap.get(cv2.CAP_PROP_POS_FRAMES) - 1)/framerate
-    data.append((time, *orangeCentroidL, *orangeCentroidR, *blueCentroid, *pinkCentroid))
+    data.append((time, *pivotCentroidL, *pivotCentroidR, *bobCentroidL, *bobCentroidR))
 
 
 # Save the position data to a csv
 with open('data/antiphasepositionsTEST.csv', 'w') as out:
     writer = csv.writer(out, delimiter=',')
-    writer.writerow(['t','orangeLx','orangeLy', 'orangeRx', 'orangeRy','bluex','bluey','pinkx','pinky'])
+    writer.writerow(['t','pivotLx','pivotLy', 'pivotRx', 'pivotRy','bobLx','bobLy','bobRx','bobRy'])
     for row in data:
         writer.writerow(row)
 
-# cleanup opencv
-cap.release()
+# Cleanup opencv
 video.release()
+cap.release()
 cv2.destroyAllWindows()
--- trackersingle.py
+++ trackersingle.py
@@ -4,10 +4,11 @@ import csv
 import colorsys
 
 # Variables to change
-videoFilename = 'videos/single.mov'
-bobRGB = np.array([56, 134, 173])
-pivotRGB = np.array([225, 163, 70])
-framerate = 30
+videoFilename = 'videos/single_test.MOV'
+bobRGB = np.array([71, 130, 174])
+pivotLRGB = np.array([214, 163, 52])
+pivotRRGB = np.array([90, 158, 107])
+framerate = 60
 dataFilename = 'data/singlepositions.csv'
 videoOutputFilename = 'output.mp4'
 
@@ -17,20 +18,23 @@ maxHSV = np.array([179, 255, 255])
 minHSV = np.array([0, 0, 0])
 
 bobHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(bobRGB/255.))), maxHSV)).astype(int)
-bobLowerBound = np.maximum(bobHSV - 40, minHSV)
-bobUpperBound = np.minimum(bobHSV + 40, maxHSV)
+bobLowerBound = np.maximum(bobHSV - 30, minHSV)
+bobUpperBound = np.minimum(bobHSV + 30, maxHSV)
 
-pivotHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(pivotRGB/255.))), maxHSV)).astype(int)
-pivotLowerBound = np.maximum(pivotHSV - 40, minHSV)
-pivotUpperBound = np.minimum(pivotHSV + 40, maxHSV)
+pivotLHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(pivotLRGB/255.))), maxHSV)).astype(int)
+pivotLLowerBound = np.maximum(pivotLHSV - 30, minHSV)
+pivotLUpperBound = np.minimum(pivotLHSV + 30, maxHSV)
 
+pivotRHSV = np.rint(np.multiply(np.array(colorsys.rgb_to_hsv(*list(pivotRRGB/255.))), maxHSV)).astype(int)
+pivotRLowerBound = np.maximum(pivotRHSV - 30, minHSV)
+pivotRUpperBound = np.minimum(pivotRHSV + 30, maxHSV)
 
 
 cap = cv2.VideoCapture(videoFilename)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 fourcc = cv2.VideoWriter_fourcc('a', 'v', 'c', '1') # Define the codec
-video = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width,height))
+video = cv2.VideoWriter('output.mp4', fourcc, framerate, (width,height))
 data = []
 
 while(cap.isOpened()):
@@ -38,7 +42,6 @@ while(cap.isOpened()):
 
     if(frame is None):
         break
-    height, width = frame.shape[:2]
 
     # if cap.get(cv2.CAP_PROP_POS_FRAMES) > 30:
     #     break
@@ -46,41 +49,39 @@ while(cap.isOpened()):
     # Create masks for each colored marker
     frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     bobMask = cv2.inRange(frameHSV, bobLowerBound, bobUpperBound)
-    pivotMask = cv2.inRange(frameHSV, pivotLowerBound, pivotUpperBound)
+    pivotLMask = cv2.inRange(frameHSV, pivotLLowerBound, pivotLUpperBound)
+    pivotRMask = cv2.inRange(frameHSV, pivotRLowerBound, pivotRUpperBound)
 
     # Image filtering
-    kernalOpen = np.ones((width//100,width//100))
-    kernalClose = np.ones((width//20,width//20))
+    kernalOpen = np.ones((width//200,width//200))
+    kernalClose = np.ones((width//100,width//100))
     bobMaskOpen = cv2.morphologyEx(bobMask,cv2.MORPH_OPEN, kernalOpen)
     bobMaskClose = cv2.morphologyEx(bobMaskOpen,cv2.MORPH_CLOSE, kernalClose)
-    pivotMaskOpen = cv2.morphologyEx(pivotMask,cv2.MORPH_OPEN, kernalOpen)
-    pivotMaskClose = cv2.morphologyEx(pivotMaskOpen,cv2.MORPH_CLOSE, kernalClose)
+    pivotLMaskOpen = cv2.morphologyEx(pivotLMask,cv2.MORPH_OPEN, kernalOpen)
+    pivotLMaskClose = cv2.morphologyEx(pivotLMaskOpen,cv2.MORPH_CLOSE, kernalClose)
+    pivotRMaskOpen = cv2.morphologyEx(pivotRMask,cv2.MORPH_OPEN, kernalOpen)
+    pivotRMaskClose = cv2.morphologyEx(pivotRMaskOpen,cv2.MORPH_CLOSE, kernalClose)
 
     bobContours = cv2.findContours(bobMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    pivotContours = cv2.findContours(pivotMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    pivotLContours = cv2.findContours(pivotLMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    pivotRContours = cv2.findContours(pivotRMaskOpen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     bobMoments = cv2.moments(bobContours[0])
-    pivotMoments1 = cv2.moments(pivotContours[1][0])
-    pivotMoments2 = cv2.moments(pivotContours[1][1])
+    pivotLMoments = cv2.moments(pivotLContours[0])
+    pivotRMoments = cv2.moments(pivotRContours[0])
 
     # Calculate centroids
     bobCentroid = (int(bobMoments['m10']/bobMoments['m00']),int(bobMoments['m01']/bobMoments['m00']))
-    pivotCentroid1 = (int(pivotMoments1['m10']/pivotMoments1['m00']),int(pivotMoments1['m01']/pivotMoments1['m00']))
-    pivotCentroid2 = (int(pivotMoments2['m10']/pivotMoments2['m00']),int(pivotMoments2['m01']/pivotMoments2['m00']))
-
-    if pivotCentroid1[0] > pivotCentroid2[0]:
-        pivotCentroidR = pivotCentroid1
-        pivotCentroidL = pivotCentroid2
-    else:
-        pivotCentroidR = pivotCentroid2
-        pivotCentroidL = pivotCentroid1
+    pivotLCentroid = (int(pivotLMoments['m10']/pivotLMoments['m00']),int(pivotLMoments['m01']/pivotLMoments['m00']))
+    pivotRCentroid = (int(pivotRMoments['m10']/pivotRMoments['m00']),int(pivotRMoments['m01']/pivotRMoments['m00']))
 
     # Create frame with marked centroids
     centroidim = frame.copy()
     cv2.circle(centroidim, bobCentroid, 3, (0,0,0), -1)
-    cv2.circle(centroidim, pivotCentroid1, 3, (0,0,0), -1)
-    cv2.circle(centroidim, pivotCentroid2, 3, (0,0,0), -1)
+    cv2.circle(centroidim, pivotLCentroid, 3, (0,0,0), -1)
+    cv2.circle(centroidim, pivotRCentroid, 3, (0,0,0), -1)
     cv2.drawContours(centroidim, bobContours[1], -1, (0,0,0), 2)
-    cv2.drawContours(centroidim, pivotContours[1], -1, (0,0,0), 2)
+    cv2.drawContours(centroidim, pivotLContours[1], -1, (0,0,0), 2)
+    cv2.drawContours(centroidim, pivotRContours[1], -1, (0,0,0), 2)
 
     # Display frame for review
     # cv2.namedWindow('review', cv2.WINDOW_NORMAL)
@@ -88,12 +89,12 @@ while(cap.isOpened()):
 
     # cv2.imshow("review", cv2.resize(frame, (0, 0), None, .5, .5))
     # cv2.waitKey(0)
-    # cv2.imshow("review", cv2.resize(pivotMask, (0, 0), None, .5, .5))
-    # cv2.waitKey(0)
-    # cv2.imshow("review", cv2.resize(pivotMaskOpen, (0, 0), None, .5, .5))
-    # cv2.waitKey(0)
-    # cv2.imshow("review", cv2.resize(pivotMaskClose, (0, 0), None, .5, .5))
-    # cv2.waitKey(0)
+    # # cv2.imshow("review", cv2.resize(pivotLMask, (0, 0), None, .5, .5))
+    # # cv2.waitKey(0)
+    # # cv2.imshow("review", cv2.resize(pivotLMaskOpen, (0, 0), None, .5, .5))
+    # # cv2.waitKey(0)
+    # # cv2.imshow("review", cv2.resize(pivotLMaskClose, (0, 0), None, .5, .5))
+    # # cv2.waitKey(0)
     # cv2.imshow("review", cv2.resize(centroidim, (0, 0), None, .5, .5))
     # cv2.waitKey(1)
 
@@ -104,7 +105,7 @@ while(cap.isOpened()):
 
     # Append positions for this frame to the data
     time = (cap.get(cv2.CAP_PROP_POS_FRAMES) - 1)/framerate
-    data.append((time, *pivotCentroidL, *pivotCentroidR, *bobCentroid))
+    data.append((time, *pivotLCentroid, *pivotRCentroid, *bobCentroid))
 
 
 # Save the position data to a csv
@@ -115,7 +116,7 @@ with open(dataFilename, 'w') as out:
         writer.writerow(row)
 
 
-# cleanup opencv
+# Cleanup opencv
 video.release()
 cap.release()
 cv2.destroyAllWindows()
